0caafa8731be87a9ce11c11221c001ac605bbc94
RichardTran <trannguyenlong1991@gmail.com>
2017-10-27 11:34:10 +0700

    commit

---
+++ Idiot/IdiotViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  DelegateTutorial
+//
+//  Created by James Rochabrun on 2/7/17.
+//  Copyright © 2017 James Rochabrun. All rights reserved.
+//
+
+import UIKit
+
+//MARK: step 4 conform the protocol here
+class IdiotViewController: UIViewController, SmartDelegate {
+    
+    @IBOutlet weak var answerIdiot: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let nav = segue.destination as? UINavigationController, let svc = nav.topViewController as? SmartViewController {
+            svc.delegate = self
+        }
+    }
+    
+    func updateAnswer(_ answer: String) {
+        answerIdiot.text = answer
+    }
+}
---
+++ Idiot/SmartViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ClassBVC.swift
+//  DelegateTutorial
+//
+//  Created by James Rochabrun on 2/7/17.
+//  Copyright © 2017 James Rochabrun. All rights reserved.
+//
+
+import UIKit
+
+//MARK: step 1 Add Protocol here
+protocol SmartDelegate: class {
+    func updateAnswer(_ answer: String)
+}
+
+class SmartViewController: UIViewController {
+    
+
+    //MARK: step 2 Create a delegate property here, don't forget to make it weak!
+    weak var delegate: SmartDelegate?
+    
+    @IBOutlet weak var isIdiot: UISwitch!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    @IBAction func dismissView(_ sender: UIBarButtonItem) {
+        delegate?.updateAnswer(isIdiot.isOn ? "You are Idiot" : "You are not Idiot")
+        navigationController?.dismiss(animated: true)
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "goBackAnswer" {
+//            delegate?.updateAnswer(isIdiot.isOn ? "You are Idiot" : "You are not Idiot")
+//        }
+//    }
+}
